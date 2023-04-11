ed5ecc29b82fc56bd99f155ed754b88c528c3082
Skymmel <65237069+Skymmel@users.noreply.github.com>
2023-04-11 21:44:33 +0200

    add

---
+++ Cuboid/Block.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Cuboid{
+    internal class Block {
+        private double A { get; }
+        private double B { get; }
+        private double C { get; }
+    
+        public Block(double a, double b, double c) {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+
+        private double Volume() {
+            return this.A * this.B * this.C;
+        }
+
+        private double Surface() {
+            return 2 * (this.A * this.B + this.B * this.C + this.A * this.C);
+        }
+
+        private double Diagonal() {
+            return Math.Sqrt(this.A * this.A + this.B * this.B + this.C * this.C);
+        }
+
+        private double WallDiagonal() {
+            return Math.Sqrt(this.A * this.A + this.B * this.B);
+        }
+
+        public void PrintResult(){
+            WriteColored("Objem", this.Volume());
+            WriteColored("Obsah", this.Surface());
+            WriteColored("Tělesová úhlopříčka", this.Diagonal());
+            WriteColored("Stěnová úhlopříčka", this.WallDiagonal(), true);
+        }
+
+        private static void WriteColored(string name, double value, bool isLast = false){
+            Console.Write($"{name}: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(value);
+            Console.ResetColor();
+            if(!isLast){
+                Console.Write(" | ");
+            }
+        }
+    }
+}
---
+++ Cuboid/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cuboid{
+    internal static class Program {
+        public static void Main(string[] args){
+            
+            // P R O G R A M  B Y  S K Y M M E L
+            // website: https://skymmel.eu
+            // github: https://github.com/Skymmel
+            // repository: https://github.com/Skymmel/Cuboid
+
+            Console.Title = "Určení parametrů | © Wilhelm Skyba";
+            Block block = new Block(GetValue("A"), GetValue("B"), GetValue("C"));
+            block.PrintResult();
+            Console.Read();
+        }
+        private static double GetValue(string side){
+            Console.Write($"Zadejte stranu {side}: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            string input = Console.ReadLine();
+            Console.ResetColor();
+            return double.Parse( input ?? "0");
+        }
+    }
+}
